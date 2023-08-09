f3c45224edb8cce17ab841319beeeda455f880d5
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-08-10 06:34:22 +0800

    Development (#14)
    * create the database configuration
    
    * create the generic SQL repository
    
    * create the project domain, generic repository, audi domain and mapper
    
    * create the project resolver
    
    * fix build errors
    
    * create the project module, domain, entity
    
    * implement the create project mutation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { validatePipeInstance } from './utils/validation-pipe-instance';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(validatePipeInstance);
   await app.listen(3000);
 }
 bootstrap();
--- apps/api/src/project/project_service.ts
+++ apps/api/src/project/project_service.ts
@@ -2,8 +2,10 @@ import { Inject } from '@nestjs/common';
 import { TYPES } from 'src/application/constants';
 import { IProjectRepository } from 'src/data_access/repositories/interfaces/project_repository.interface';
 import { Project } from './project';
+import { Audit } from 'src/audit/audit';
+import { IProjectService } from './interfaces/project_service.interface';
 
-export class ProjectService {
+export class ProjectService implements IProjectService {
   constructor(
     @Inject(TYPES.projectRepository)
     private readonly projectRepository: IProjectRepository,
@@ -11,5 +13,19 @@ export class ProjectService {
 
   async findProjects(): Promise<Project[]> {
     return await this.projectRepository.findProjects();
+  }
+
+  async createProject(request: {
+    name: string;
+    code: string;
+    description: string;
+    category: string;
+  }): Promise<Project> {
+    const audit: Audit = Audit.create({
+      auditCreatedDateTime: new Date().toISOString(),
+      auditCreatedBy: 'Ola',
+    }).getValue();
+    const project = Project.create({ ...request, audit }).getValue();
+    return await this.projectRepository.save(project);
   }
 }
