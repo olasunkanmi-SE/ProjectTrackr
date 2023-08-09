be1f7bcaaa5e902e2ce909cbbcef6ed55af37253
ola <raymond.oyinlola@gmail.com>
2023-08-10 07:49:41 +0800

    create the issue datamodel and graphql schema

--- apps/api/src/data_access/model/project.entity.ts
+++ apps/api/src/data_access/model/project.entity.ts
@@ -1,10 +1,15 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { BaseModel } from 'src/data_access/model/base.entity';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { IssueDataModel } from './issue.entity';
+import { IProject } from 'src/project/project.interface';
 
 @Entity({ name: 'projects' })
 @ObjectType()
-export class ProjectDataModel extends BaseModel {
+export class ProjectDataModel
+  extends BaseModel
+  implements Omit<IProject, 'audit'>
+{
   @Field()
   @Column({ type: 'varchar', length: 128 })
   name: string;
@@ -20,4 +25,8 @@ export class ProjectDataModel extends BaseModel {
   @Field()
   @Column({ type: 'varchar', length: 32 })
   category: string;
+
+  @OneToMany(() => IssueDataModel, (issue) => issue.project)
+  @Field(() => [IssueDataModel], { nullable: true })
+  issues?: IssueDataModel[];
 }
