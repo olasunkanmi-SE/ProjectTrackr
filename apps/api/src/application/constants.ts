66bf5a42e7ec0dd34887ad8be8be6ccab1db61d3
Oyinlola Olasunkanmi Raymond <60177090+olasunkanmi-SE@users.noreply.github.com>
2023-08-10 08:06:29 +0800

    Development (#16)
    * create the database configuration
    
    * create the generic SQL repository
    
    * create the project domain, generic repository, audi domain and mapper
    
    * create the project resolver
    
    * fix build errors
    
    * create the project module, domain, entity
    
    * implement the create project mutation
    
    * create the issue datamodel and graphql schema

--- apps/api/src/application/constants.ts
+++ apps/api/src/application/constants.ts
@@ -2,3 +2,17 @@ export const TYPES = {
   projectRepository: 'projectRepository',
   projectService: 'projectService',
 };
+
+export enum IssueStatus {
+  REOPENED = 'Reopened',
+  INPROGRESS = 'In progress',
+  RESOLVED = 'Resolved',
+  CLOSED = 'Closed',
+  READYFORDEVELOPMENT = 'Ready for development',
+}
+
+export enum IssuePriority {
+  HIGH = 'high',
+  MEDIUM = 'medium',
+  LOW = 'low',
+}
---
+++ apps/api/src/data_access/model/issue.entity.ts
@@ -0,0 +1,49 @@
+import { IssueStatus, IssuePriority } from './../../application/constants';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { BaseModel } from './base.entity';
+import { ProjectDataModel } from './project.entity';
+
+@Entity({ name: 'issues' })
+@ObjectType()
+export class IssueDataModel extends BaseModel {
+  @Field()
+  @Column({ type: 'varchar', length: 128 })
+  title: string;
+
+  @Field()
+  @Column({ type: 'text' })
+  description: string;
+
+  @Field()
+  @Column({
+    type: 'enum',
+    enum: IssueStatus,
+    default: IssueStatus.READYFORDEVELOPMENT,
+  })
+  status: string;
+
+  @Field()
+  @Column({
+    type: 'enum',
+    enum: IssuePriority,
+    default: IssuePriority.LOW,
+  })
+  priority: string;
+
+  @Field({ nullable: true })
+  @Column({ nullable: true, length: 128 })
+  assignee?: string;
+
+  @Field({ nullable: true })
+  @Column({ nullable: true, length: 128 })
+  reporter?: string;
+
+  @Field()
+  @Column()
+  projectId: string;
+
+  @ManyToOne(() => ProjectDataModel, (project) => project.issues)
+  @Field(() => ProjectDataModel)
+  project: ProjectDataModel;
+}
